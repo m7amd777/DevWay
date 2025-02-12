b8279dee2a1839902da3890fd76657f1f385bbd8
zanasser <zainabnasser010@gmail.com>
2025-02-12 15:26:05 +0300

    ..

--- js/script.js
+++ js/script.js
@@ -34,3 +34,18 @@ function createMatrixBackground() {
 
 window.addEventListener('load', createMatrixBackground);
 window.addEventListener('resize', createMatrixBackground);
+
+function toggleTheme() {
+    const html = document.documentElement;
+    const currentTheme = html.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    
+    html.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+}
+
+// Set Initial Theme
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme') || 'dark';
+    document.documentElement.setAttribute('data-theme', savedTheme);
+});
